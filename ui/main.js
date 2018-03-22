ebc44259c9d8852954872ae5cf81b8c7f3db1472
Abhinay-Sajwan007 <abhinay.sajwan@gmail.com>
2018-03-22 12:42:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,10 +5,27 @@ var element = document.getElementById('main-text');
 
 element.innerHTML = 'New Value'
 
-//move the image
 
 var img = document.getElementById('madi');
-
+//move the image
+/*
 img.onclick = function(){
     img.style.marginLeft='100px'
 }
+*/
+
+
+// animation
+
+var marginleft = 0;
+
+function moveRight(){
+    
+    marginleft = marginleft + 10;
+    img.style.marginLeft = marginleft+'px';
+    
+}
+img.onclick = function(){
+    var interval = setInterval(moveRight,50)
+}
+
