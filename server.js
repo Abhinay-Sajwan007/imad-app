1563a6f2ea61af5f06e47d4bc8f8b3a780f63ad0
Abhinay-Sajwan007 <abhinay.sajwan@gmail.com>
2018-03-22 23:27:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,6 +74,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function (req, res) {
+    counter = counter + 1;
+  res.send(counter.toString());
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -93,11 +99,7 @@ app.get('/:articleName', function (req, res) {
     res.send(createTemplate(articles[articleName]));
 });
 
-var counter = 0;
-app.get('/counter', function (req, res) {
-    counter = counter + 1;
-  res.send(counter.toString());
-});
+
 /*
 var names = [];
 app.get('/submit-name/:name',function(req,res){
