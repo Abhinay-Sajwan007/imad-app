f74bcd98c8e8b510264316c1b135eb26498a6076
Abhinay-Sajwan007 <abhinay.sajwan@gmail.com>
2018-03-27 22:50:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 var express = require('express');
 var morgan = require('morgan');
+var Pool = require('pg').Pool;
 var path = require('path');
 
+var config = {
+    user:'abhinaysajwan',
+    database : 'abhinaysajwan',
+    host : 'http://db.imad.hasura-app.io',
+    port : '5432',
+    password : process.env.DB_PASSWORD
+};
+
+ 
 var app = express();
 app.use(morgan('combined'));
 
@@ -74,10 +84,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool = new Pool(config);
+
 app.get('/test-db',function(){
     //make  a select request 
     
     //return a response with a results
+    pool.query('SELECT * FROM test',function(err,result){
+       if(err){
+           res.status(500).sen(err.toString());
+       } else{
+           res.send(JSON.stringify(result));
+       }
+    });
 });
 
 var counter = 0;
