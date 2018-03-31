178503d585e916403c4ea41bf62fb47ea903678d
Abhinay-Sajwan007 <abhinay.sajwan@gmail.com>
2018-04-01 00:51:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -145,7 +145,7 @@ app.post('/login',function(req,res){
     else
        {
            if(result.rows.length === 0){
-               res.status(403).send('Username/Password is invalid');
+               res.send(403).send('Username/Password is invalid');
            }
            else
            {
