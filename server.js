4c94811df90c2133cacdf5007b8706decd57fdad
Abhinay-Sajwan007 <abhinay.sajwan@gmail.com>
2018-03-31 21:56:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -133,7 +133,7 @@ app.post('/login',function(req,res){
     var username = req.body.username;
     var password = req.body.password;
    
-    pool.query('SELECT * FROM "user" WHERE username = $1', [username] ,function(err,result){
+    pool.query('SELECT * FROM "user" WHERE username = ($1)', [username] ,function(err,result){
      if(err)
        {
            res.status(500).send(err.toString());
@@ -179,12 +179,12 @@ app.get('/test-db',function(req,res){
     });
 });
 
-var counter = 0;
+/*var counter = 0;
 app.get('/counter', function (req, res) {
     counter = counter + 1;
   res.send(counter.toString());
 });
-
+*/
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -211,6 +211,8 @@ app.get('/submit-name/:name',function(req,res){
 
 */
 
+
+/*
 // using query parameter (in urls)
 
 var names = [];
@@ -248,7 +250,7 @@ app.get('/articles/:articleName', function (req, res) {
     });
     
 });
-
+*/
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
@@ -257,12 +259,6 @@ var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
-
-
-
-
-
-
 
 /*
 app.get('/article-two', function (req, res) {
